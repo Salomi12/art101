7f16f71615760f341ae25e364a7478a1c0a7eae6
Salomi Galodamu <sgalodam@ucsc.edu>
2023-05-15 22:09:47 -0700

    Final lab 6 page
    includes arrays and objects

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,51 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
 
-// Constants
+/** 
+// index.js - Lab 6: Objects and Arrays
+// Author: Salomi Galodamu
+// Date: May 14 2023
+**/
+
+// Define Varialbes
+myTransport = ["bicyle", "car" , "bus" , "ferry" , "amtrak" ];
+
+// Create an Object for my main ride
+myMainRide = {
+  make: "Volkswagen",
+  model: "Polo",
+  color:"white",
+  year: 2009, 
+  age: function () {
+    return 2023 - this.year;
+  }
+}
+
+// Script Output 
+
+console.log(myTransport);
+console.log(myMainRide);
+
+//Calculate 
+age= 2023 - 2009;
+
+    
+document.writeln(" Types of transportation I use: " ,  myTransport , "</br>");
+document.writeln("My Main Ride: <pre>" ,
+    JSON.stringify(myMainRide, null, '\t'), "</pre");
+
+
+
+                
+                
+
+
+
+
+
+
+
+ 
+    
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
