c9c25ab2d17375231d1dd1d99a2c90402944cc63
Salomi Galodamu <sgalodam@ucsc.edu>
2023-06-08 04:14:19 -0700

    final lab 12
    new

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -1,42 +1,38 @@
 
 /** 
-// index.js - Lab 9: Anon and Callbacks
+// index.js - Lab 12: Conditionals
 // Author: Salomi Galodamu
-// Date: May 17 2023
+// Date: June 2 2023
 **/
 
-function shuffleArray(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+//returning houses: Hufflepuff, Gryffindor, Ravenclaw, Slytherin
+function sortingHat(str){
+    len= str.length;
+    mod= len % 4;
+    if (mod==0) {
+      return "Hufflepuffer"
+    }
+    else if (mod==1) {
+      return "GryffinDori"
+    }
+    else if (mod=2) {
+      return "Ravenclawrette"
+    }
+    else if (mod=3)
+      return "Slytherinsss"
   }
-  return array;
-}
 
-//nameSort() functions from lab 7
-function reorderUserName(word) {
-  var wordArray = word.toLowerCase().split('');
-  var newArray = shuffleArray(wordArray);
-  return newArray.join('');
-}
-// return string in Title Case
-// aid from https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
-function toTitleCase(str) {
-  return str.replace(
-      /\w\S*/g,
-      function(txt) {
-          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-      }
-  );
-}
+//attach link listener and results to sortingHat(name)
+  $("#button").click(function(){
+    var name =document.getElementById("input").value;
+    var house = sortingHat(name);
+    newText = "<p> Sorting Hat Says " + house + "<p>";
+    document.getElementById("output").innerHTML = newText;
+  })
+
+
+
+
 
 
 
